fa04385d4b279aba24ce25f83edc9a3897e2d69d
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-01-23 21:01:23 +0000

    added function that updates the time blocks to change color dependent on time. Also updated CSS file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -26,21 +26,21 @@ var timeBlockLi = document.getElementById("timeBlockLi");
 var currentDayShow = document.getElementById("currentDay");
 //will show the different times at the side of each calendar row.
 var timeBlockInput = ["08:00am", "09:00am", "10:00am", "11:00am", "12:00pm", "13:00pm", "14:00pm",
-"15:00pm", "16:00pm", "17:00pm", "18:00pm"];
+    "15:00pm", "16:00pm", "17:00pm", "18:00pm"];
 
 
-function CurrentDateTime () {
+function CurrentDateTime() {
     var currentDT = moment().format('MMMM Do YYYY, h:mm:ss a');
     currentDayShow.innerHTML = currentDT;
     currentDayShow.innerHTML = "Today's Date: " + currentDT;
-} 
+}
 setInterval(CurrentDateTime, 1000);
 
 
 //this function gets all of the times stated in the timeBlockInput array above and 
 //displays thehe list on the page.
-getTimeBlock ();
-function getTimeBlock () {
+getTimeBlock();
+function getTimeBlock() {
     timeBlockLi.innerHTML = "";
 
     for (var i = 0; i < timeBlockInput.length; i++) {
@@ -70,11 +70,53 @@ function getTimeBlock () {
 }
 
 // the function below will allow the user to save their tasks. 
-    $(document).on('click', '.saveTask', function () {
-    var saveUserTask  = $(this).val();
+$(document).on('click', '.saveTask', function () {
+    var saveUserTask = $(this).val();
     var userDescription = document.getElementById(saveUserTask).value;
     localStorage.setItem(saveUserTask, userDescription);
 });
 
-//This function will update the color of the time blocks whether they are in the past, present or future: 
+// This function will update the color of the time blocks whether they are in the past, present or future: 
+function colorTimeBlocks () {
+    var currentTime = moment().format('h a');
+    var hourNow = moment(currentTime, 'h a');
+    var setTask = document.getElementsByClassName('description')
+
+    for (var i = 0; i < setTask.length; i++) {
+        var block = moment(timeBlockInput[i], 'h a');
+        if (hourNow.isSame(block) === true) {
+            setTask[i].classList.add('present')
+            setTask[i].classList.remove('future')
+            setTask[i].classList.remove('past')
+        } else if (hourNow.isBefore(block) === true) {
+            setTask[i].classList.add('future')
+            setTask[i].classList.remove('past')
+            setTask[i].classList.remove('present')
+        } else if (hourNow.isBefore(block) === false) {
+            setTask[i].classList.add('past')
+            setTask[i].classList.remove('future')
+            setTask[i].classList.remove('present')
+        }
+    }
+}
+colorTimeBlocks()
+setInterval(colorTimeBlocks, 10000);
+
+
+
+//This will allow the user to clear all of the text areas. 
+// function clearAll() {
+//     var clearConfirm = confirm("Do you really want to delete all of your tasks?");
+//     var setTask = document.getElementsByClassName('description');
+
+//     if (clearConfirm === true) {
+//         for (var i = 0; i < setTask.length; i++) {
+//             localStorage.removeItem(i);
+//         }
+//         setTask.innerText = "";
+//         getTimeBlock();
+//         colorTimeBlocks();
+
+//     }
+// }
 
